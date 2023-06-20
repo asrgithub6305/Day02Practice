0a8d4fffcffc4a3e52aeb917eb06642719c8f28a
assrrr <asr1997ktq@gmail.com>
2023-06-20 11:53:26 +0530

    Arithmetic Operator in Java

---
+++ Day02Practice/src/com/bridgelab/day02practice/ArithmeticOperator.java
@@ -0,0 +1,15 @@
+package com.bridgelab.day02practice;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 5;
+        int c= 2;
+        System.out.println("a+b*c ="+ (a+b*c));
+        System.out.println("c+a/b ="+ (c+a/b));
+        System.out.println("a%b+c ="+ (a%b+c));
+        System.out.println("a*b+c ="+ (a*b+c));
+    }
+
+
+}
